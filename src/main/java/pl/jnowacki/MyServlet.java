0f30749e8bebdcb268c64f2a9e55e9e5e7d1bf88
Jedrzej Nowacki <contact@jnowacki.pl>
2019-10-12 07:32:29 +0200

    Theme setup

--- src/main/java/pl/jnowacki/MyServlet.java
+++ src/main/java/pl/jnowacki/MyServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("")
+@WebServlet("/hello")
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
